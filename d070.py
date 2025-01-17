a478def3fca9d72c1aa2b4d493d8f8158ce7f0dc
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-01-17 18:21:49 -0300

    Algumas outras alterações

---
+++ d070.py
@@ -0,0 +1,3 @@
+'''Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário
+vai continuar. No final, mostre: qual é o total gasto na compra, quantos produtos custam mais de 1000,
+e qual é o nome do produto mais barato.'''
---
+++ d071.py
@@ -0,0 +1,4 @@
+'''Crie um programa que simule o funcionamento de um caixa eletrônico.
+No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro)
+e o programa informará quantas cédulas de cada valor serão entregues.
+Obs: considere que o caixa possui cédulas de 50, 20, 10 e 1.'''
