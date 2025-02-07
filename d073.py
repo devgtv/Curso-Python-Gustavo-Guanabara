c7997bd7d5dd2f349fc0f7e0c69a47e4f6a435ad
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-02-06 21:52:36 -0300

    Poucas alterações

--- d073.py
+++ d073.py
@@ -12,19 +12,14 @@ times = (
 )
 
 
-# Exemplo de
 print('='*20)
-print(f'Os primeiros 5 times colocados são : ')
-
-for cont in range(0,5) :
-    print(f'{times[cont]} ')
-
+print(f'Os primeiros 5 times colocados são : {times[0:5]}') # Mostre os 5 primerios colocados 
 print('='*20)
-print(f'Os ultímos 4 colocados da tabela')
-for cont in range(-4,0) : 
-    print(f'{times[cont]} ')
+print(f'Os ultímos 4 colocados  são : {times[-4:]}') # Mostre os 4 ultímos colocados da tabela.
+print('='*20)
+print(f'Times em ordem alfabética: {sorted(times)}') # Mostre os times em ordem alfabética .
+print('='*20)
 
-
-localizador = str(input('Qual time que deseja localizar ? '))
-posicao_time = times.index(localizador)
+localizador = str(input('Qual time que deseja localizar ? ')) # variavel responsavel por colher o dado do time a ser localizado.
+posicao_time = times.index(localizador) # mostre a possição dentro da tupla aonde o time do usuario se encontra
 print(f'O time {localizador} está na posição {posicao_time + 1}')
