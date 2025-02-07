f71d9c522cd62822e6b9ff1241da0c30849490b0
devgtv <gustavobarrosfialho1402gb@gmail.com>
2025-02-06 21:34:10 -0300

    Algumas alterações

--- d072.py
+++ d072.py
@@ -2,7 +2,12 @@
 O seu programa deverá ler um número pelo teclado entre 0 e 20  
 e exibi-lo por extenso.'''  
 
+num = ('Zero', 'Um','Dois','Tres','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Catorze','Quinze','Dezesseis','dezessete','Dezoito','Dezonove','Vinte')
+while True : 
 
-
-
- 
+    n = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= n and n <=20 : 
+        print(f'Digitate o número: {num[n]}') 
+    continuar = str(input('Deseja continuar [S/N]')).upper().strip()[0]
+    if continuar in 'N': 
+        break
--- d073.py
+++ d073.py
@@ -3,4 +3,28 @@ Depois, mostre:
 - Apenas os 5 primeiros colocados  
 - Os últimos 4 colocados da tabela  
 - Uma lista com os times em ordem alfabética  
-- Em que posição na tabela está o time da Chapecoense?'''  
+- Em que posição na tabela está o time da Goias?'''  
+times = (
+    "Botafogo", "Palmeiras", "Flamengo", "Fortaleza", "Internacional", "São Paulo",
+    "Corinthians", "Bahia", "Cruzeiro", "Vasco da Gama", "Vitória", "Atlético Mineiro",
+    "Fluminense", "Grêmio", "Juventude", "Bragantino", "Athletico Paranaense", "Cuiabá",
+    "Goiás", "América Mineiro"
+)
+
+
+# Exemplo de
+print('='*20)
+print(f'Os primeiros 5 times colocados são : ')
+
+for cont in range(0,5) :
+    print(f'{times[cont]} ')
+
+print('='*20)
+print(f'Os ultímos 4 colocados da tabela')
+for cont in range(-4,0) : 
+    print(f'{times[cont]} ')
+
+
+localizador = str(input('Qual time que deseja localizar ? '))
+posicao_time = times.index(localizador)
+print(f'O time {localizador} está na posição {posicao_time + 1}')
